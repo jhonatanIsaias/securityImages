342e45862868b39651742f8d3a12aa51318c603d
jhonatan <jhonatan.santos13@academico.ifs.edu.br>
2023-09-11 16:06:47 -0300

    projeto inicializado

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import StackComponent from './src/navigation/stack';
+export default function App() {
+  return <StackComponent  />;
+}
+
