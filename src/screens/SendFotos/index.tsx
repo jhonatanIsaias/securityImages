93da9f020e6e7f0ca5a8fbf301f606459adc0f72
Kaylaineasb <kaylaineasb9@gmail.com>
2023-09-19 15:16:35 -0300

    adionando input da data

--- src/screens/SendFotos/index.tsx
+++ src/screens/SendFotos/index.tsx
@@ -1,12 +1,19 @@
-import { View, Text, TouchableOpacity, TextInput} from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity, TextInput,KeyboardAvoidingView,
+    Platform,Modal} from "react-native";
+import React from 'react';
+import useState from 'react';
 import { StyleSheet } from "react-native";
 import { useFonts, Quicksand_300Light, Quicksand_400Regular, Quicksand_500Medium, Quicksand_600SemiBold, Quicksand_700Bold } from '@expo-google-fonts/quicksand';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Icon } from "react-native-elements";
+//import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
-import DateTimePicker from'@react-native-community/datetimepicker';
-import {useState, useEffect} from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { SetStateAction} from "react";
+import DatePicker from "react-native-modern-datepicker";
+import { getFormatedDate} from "react-native-modern-datepicker";
+
+
+
 
 const SendFotos = () => {
 
@@ -22,9 +29,24 @@ const SendFotos = () => {
     if (!fontLoaded) {
         return null;
     }
+    
     const navigation = useNavigation();
-    const [date, setDate] = useState(new Date());
-    //const [showPicker,setShowPicker] = useState(false);
+    const [openStartDatePicker, setOpenStartDatePicker] = useState(false);
+  const today = new Date();
+  const startDate = getFormatedDate(
+    today.setDate(today.getDate() + 1),
+    "DD/MM/YYYY"
+  );
+  const [selectedStartDate, setSelectedStartDate] = useState("");
+  const [startedDate, setStartedDate] = useState("");
+
+  function handleChangeStartDate(propDate) {
+    setStartedDate(propDate);
+  }
+
+  const handleOnPressStartDate = () => {
+    setOpenStartDatePicker(!openStartDatePicker);
+  };
     return (
         <LinearGradient colors={['#9370DB', '#C7B6EA']} style={{ flex: 1 }}>
             <View style={{flex:0.2}}>
@@ -38,14 +60,62 @@ const SendFotos = () => {
                 <TextInput style={ScreenStyles.inputDesc}></TextInput>
                 <Text style={ScreenStyles.title}>Data:</Text>
 
-                <DateTimePicker 
-                 mode="date"
-                 display="spinner"
-                 value={date}
+                <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "android" ? "padding" : ""}
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#fff",
+        }}
+      >
+        <View style={{ flex: 1, alignItems: "center" }}>
+
+
+          <View style={{ width: "100%", paddingHorizontal: 22, marginTop: 64 }}>
+            <View>
+              <TouchableOpacity
+                style={ScreenStyles.inputBtn}
+                onPress={handleOnPressStartDate}
+              >
+                <Text>{selectedStartDate}</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={openStartDatePicker}
+          >
+            <View style={ScreenStyles.centeredView}>
+              <View style={ScreenStyles.modalView}>
+                <DatePicker
+                  mode="calendar"
+                  minimumDate={startDate}
+                  selected={startedDate}
+                  onDateChanged={handleChangeStartDate}
+                  onSelectedChange={(date: SetStateAction<string>) => setSelectedStartDate(date)}
+                  options={{
+                    backgroundColor: "#FFFFFF",
+                    textHeaderColor: "#865FD6",
+                    textDefaultColor: "#865FD6",
+                    selectedTextColor: "#865FD6",
+                    mainColor: "865FD6",
+                    textSecondaryColor: "#865FD6",
+                    format: 'DD/MM/YYYY'
+                  }}
                 />
-                <TextInput placeholder="dd/MM/yyyy" style={ScreenStyles.input}></TextInput>
-              
-               
+                <TouchableOpacity onPress={handleOnPressStartDate}>
+                  <Text style={{ color: '#865FD6' }}>Fechar</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
             </View>
             <View style={ScreenStyles.containerButtons}>
                 <TouchableOpacity style={ScreenStyles.button} onPress={() => {
@@ -121,7 +191,44 @@ const ScreenStyles = StyleSheet.create({
     },
     containerButtons:{
         flexDirection: 'row'
-    }
+    },
+    dateComponent:{
+        width:350,
+        color: '#865FD6',
+        backgroundColor: '#FFFFF'
+    },
+    textHeader: {
+        fontSize: 36,
+        marginVertical: 60,
+        color: "#111",
+      },
+      textSubHeader: {
+        fontSize: 25,
+        color: "#111",
+      },
+      inputBtn: {
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: "#222",
+        height: 50,
+        paddingLeft: 8,
+        fontSize: 18,
+        justifyContent: "center",
+        marginTop: 14,
+      },
+      centeredView: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      modalView: {
+        margin: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 20,
+        padding: 35,
+        width: "90%",
+      }
 });
 
 export default SendFotos;
