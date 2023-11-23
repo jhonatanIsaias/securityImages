07b992d911a147eb24580f0329456e604d73400b
jhonatan <jhonatan.santos13@academico.ifs.edu.br>
2023-11-23 07:52:22 -0300

    design da camera auterado

--- src/components/Camera/index.tsx
+++ src/components/Camera/index.tsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Button, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Camera } from 'expo-camera';
 import { useNavigation } from "@react-navigation/native";
+import { Feather } from '@expo/vector-icons';
 
 export default function OpenCamera() {
-  let camera : any;
+  let camera: any;
   camera = Camera;
   const navigation = useNavigation();
   const [hasPermission, setHasPermission] = useState<boolean>(false);
-  //const [camera, setCamera] = useState<any>(null);
   const [photo, setPhoto] = useState<any>(null);
   const [type, setType] = useState(camera.Constants.Type.back);
-
 
   useEffect(() => {
     (async () => {
@@ -24,19 +23,19 @@ export default function OpenCamera() {
   const snap = async () => {
     if (camera) {
       const photoData = await camera.takePictureAsync();
-     
       setPhoto(photoData.uri);
     }
   };
 
-  if (hasPermission === null) {
-    return (
-      <View>
-        <Text>No access to camera</Text>
-      </View>
-    );
-  }
-  if (hasPermission === false) {
+  const handleRetake = () => {
+    setPhoto(null);
+  };
+
+  const handleSend = () => {
+    navigation.navigate('SendFotos', { photo: photo });
+  };
+
+  if (hasPermission === null || hasPermission === false) {
     return (
       <View>
         <Text>No access to camera</Text>
@@ -48,41 +47,80 @@ export default function OpenCamera() {
     <View style={{ flex: 1 }}>
       {photo ? (
         <View style={{ flex: 1 }}>
-          <Button title="tirar foto novamente" onPress={() => setPhoto(null)} />
-          <Button title="Enviar" onPress={() => navigation.navigate('SendFotos',{photo:photo})} />
+          <Image source={{ uri: photo }} style={{ flex: 1 }} />
+          
+          <TouchableOpacity style={styles.button} onPress={handleRetake}>
+            <Feather name="rotate-cw" size={24} color="white" />
+            <Text style={styles.buttonText}>Tirar Novamente</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleSend}>
+            <Feather name="send" size={24} color="white" />
+            <Text style={styles.buttonText}>Enviar</Text>
+          </TouchableOpacity>
         </View>
       ) : (
         <SafeAreaView style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={type} ref={ref => camera = ref}>
-            <View style={{flex:10}}>
-
-            </View>
-            <View
-              style={{
-                flex: 1,
-                
-                backgroundColor: 'transparent',
-               
-              }}
-            >
-              <Button
-              
-                title="Flip"
-                onPress={() => {
-                  setType(
-                    type === camera.Constants.Type.back
-                      ? camera.Constants.Type.front
-                      : camera.Constants.Type.back
-                  );
-                }}
-              />
-              <Button title="Tirar foto" onPress={snap} />
+          <Camera style={{ flex: 1 }} type={type} ref={(ref) => (camera = ref)}>
+            <View style={{ flex: 1 }} />
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.flipButton} onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}>
+                <Feather name="rotate-cw" size={24} color="white" />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.captureButton} onPress={snap}>
+                <View style={styles.captureIcon} />
+              </TouchableOpacity>
             </View>
           </Camera>
         </SafeAreaView>
       )}
     </View>
-
-    
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+    marginBottom: 20,
+    backgroundColor: 'transparent',
+  },
+  flipButton: {
+    padding: 15,
+  },
+  captureButton: {
+    backgroundColor: 'white',
+    borderRadius: 50,
+    padding: 10,
+    marginRight: 140,
+    borderWidth: 2,
+    borderColor: 'transparent', // removido "none"
+  },
+  captureIcon: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#865FD6',
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#865FD6',
+    borderRadius: 25,
+    padding: 10,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: 'white',
+    marginLeft: 10,
+    fontSize: 16,
+  },
+});
--- src/screens/SendFotos/index.tsx
+++ src/screens/SendFotos/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, TextInput} from "react-native";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
 import React, { useState } from 'react';
 import { StyleSheet } from "react-native";
 import { useFonts, Quicksand_300Light, Quicksand_400Regular, Quicksand_500Medium, Quicksand_600SemiBold, Quicksand_700Bold } from '@expo-google-fonts/quicksand';
@@ -7,212 +7,205 @@ import DateScreen from "../../components/Date";
 import { useNavigation } from "@react-navigation/native";
 
 
-const SendFotos = ({route} : any) => {
-    const navigation = useNavigation();
-    const {photo} = route.params;
-    const [clientToken,setClientToken] = useState('');
-    const [tags,setTags] = useState('');
-    const [description, setDescription] = useState('');
-    const [date,setDate] = useState('');
+const SendFotos = ({ route }: any) => {
+  const navigation = useNavigation();
+  const { photo } = route.params;
+  const [clientToken, setClientToken] = useState('');
+  const [tags, setTags] = useState('');
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState('');
 
-    const [errorClientToken,setErrorClientToken] = useState('');
-    const [errorTags,setErrorTags] = useState('');
-    const [errorDescription, setErrorDescription] = useState('');
-    const [errorDate,setErrorDate] = useState('');
-    const [flag , setFlag] = useState(false);
-    
-    const handleDateSelect = (selectedDate:string) => {
-        setDate(selectedDate);
+
+  const handleDateSelect = (selectedDate: string) => {
+    setDate(selectedDate);
+  };
+
+  const verifyForm = () => {
+    return clientToken.trim() !== '' && tags.trim() !== '' && description.trim() !== '' && date.trim() !== ''
+  }
+
+
+  const sendRequestSaveImage = async (clientToken: string, tags: string, description: string, date: string, uri: string) => {
+
+
+    const file = new FormData();
+
+    const imageConfig = {
+      uri: uri,
+      type: 'image/jpg',
+      name: 'file.jpg',
     };
-   
-    const verifyForm = ()=>{
-      return clientToken.trim() !== '' && tags.trim() !== '' && description.trim() !== '' &&  date.trim() !== ''
-        
-      
-    }
- 
-   
-    const sendRequestSaveImage = async (clientToken:string,tags:string,description:string,date:string,uri:string) =>{
-     
-    
-        const file = new FormData();
 
-        const imageConfig = {
-          uri: uri,
-          type: 'image/jpg',
-          name: 'file.jpg',
-        };
-        
-        file.append('file',imageConfig)
-        file.append('clientToken',clientToken);
-        file.append('tags',tags);
-        file.append('description',description);
-        file.append('date',date);
-       
-             
-        return await fetch('http://192.168.0.103:3000',{
-          method: 'POST',
-          headers:{
-            'Content-Type':'multipart/form-data'
-          },
-          body: file
-          }).then(response => console.log(response));
-        
-     
- 
-    }
-   
-    const [fontLoaded] = useFonts({
+    file.append('file', imageConfig)
+    file.append('clientToken', clientToken);
+    file.append('tags', tags);
+    file.append('description', description);
+    file.append('date', date);
 
-        Quicksand_300Light,
-        Quicksand_400Regular,
-        Quicksand_500Medium,
-        Quicksand_600SemiBold, 
-        Quicksand_700Bold
-    });
 
-    if (!fontLoaded) {
-        return null;
-    }
-   
-   
-    return (
-      
-        <LinearGradient colors={['#9370DB', '#C7B6EA']} style={{ flex: 1 }}>
-            <View style={{flex:0.2}}>
-            </View>
-            <View style={ScreenStyles.containerForm}>
-                <Text style={ScreenStyles.title}>Cliente Token:</Text>
-                <TextInput style={ScreenStyles.input} onChangeText={value => setClientToken(value)}></TextInput>
-                
-                <Text style={ScreenStyles.title}>Tags:</Text>
-                <TextInput style={ScreenStyles.input} onChangeText={value => setTags(value)}></TextInput>
-              
-                <Text style={ScreenStyles.title}>Descrição:</Text>
-                <TextInput style={ScreenStyles.inputDesc} onChangeText={value => setDescription(value)}></TextInput>
-                
-                <DateScreen onDateSelect={handleDateSelect} />
-                
-                
-                </View>
-            <View style={ScreenStyles.containerButtons}>
-        <TouchableOpacity disabled={!verifyForm()}   style={[
-    ScreenStyles.button,
-    { backgroundColor: !verifyForm() ? '#A7A7A7' : '#865FD6' },
-  ]} onPress={  () =>  {
-          sendRequestSaveImage(clientToken,tags,description,date,photo);
+    return await fetch('http://192.168.0.103:3000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      body: file
+    }).then(response => console.log(response));
+
+
+
+  }
+
+  const [fontLoaded] = useFonts({
+
+    Quicksand_300Light,
+    Quicksand_400Regular,
+    Quicksand_500Medium,
+    Quicksand_600SemiBold,
+    Quicksand_700Bold
+  });
+
+  if (!fontLoaded) {
+    return null;
+  }
+
+
+  return (
+
+    <LinearGradient colors={['#9370DB', '#C7B6EA']} style={{ flex: 1 }}>
+      <View style={{ flex: 0.2 }}>
+      </View>
+      <View style={ScreenStyles.containerForm}>
+        <Text style={ScreenStyles.title}>Cliente Token:</Text>
+        <TextInput style={ScreenStyles.input} onChangeText={value => setClientToken(value)}></TextInput>
+
+        <Text style={ScreenStyles.title}>Tags:</Text>
+        <TextInput style={ScreenStyles.input} onChangeText={value => setTags(value)}></TextInput>
+
+        <Text style={ScreenStyles.title}>Descrição:</Text>
+        <TextInput style={ScreenStyles.inputDesc} onChangeText={value => setDescription(value)}></TextInput>
+
+        <DateScreen onDateSelect={handleDateSelect} />
+
+
+      </View>
+      <View style={ScreenStyles.containerButtons}>
+        <TouchableOpacity disabled={!verifyForm()} style={[
+          ScreenStyles.button,
+          { backgroundColor: !verifyForm() ? '#A7A7A7' : '#865FD6' },
+        ]} onPress={() => {
+          sendRequestSaveImage(clientToken, tags, description, date, photo);
           navigation.navigate('Home');
         }}>
           <Text style={ScreenStyles.buttonText}>Enviar</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={ScreenStyles.button} onPress={() => {}}>
+        <TouchableOpacity style={ScreenStyles.button} onPress={() => { }}>
           <Text style={ScreenStyles.buttonText}>Cancelar</Text>
-       
+
         </TouchableOpacity>
       </View>
 
-        </LinearGradient>
-    )
+    </LinearGradient>
+  )
 }
 const ScreenStyles = StyleSheet.create({
-    photoContainer: {
-        flex:1,
-        backgroundColor: "#6935D3",
-        width: '50%',
-        height: '70%',
-        alignSelf: "center",
-        marginTop: "15%",
-        borderRadius: 20
-    },
-    button: {
-        flex:1,
-        backgroundColor: '#865FD6',
-        width: '30%',
-        alignSelf: "center",
-        borderRadius: 4,
-        paddingVertical: 8,
-        marginTop: 14,
-        marginRight: 20,
-        marginLeft:50,
-        marginBottom:50,
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    buttonText: {
-        fontFamily: 'Quicksand_700Bold',
-        color: '#FFF',
-        fontSize: 18,
-      
+  photoContainer: {
+    flex: 1,
+    backgroundColor: "#6935D3",
+    width: '50%',
+    height: '70%',
+    alignSelf: "center",
+    marginTop: "15%",
+    borderRadius: 20
+  },
+  button: {
+    flex: 1,
+    backgroundColor: '#865FD6',
+    width: '30%',
+    alignSelf: "center",
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 14,
+    marginRight: 20,
+    marginLeft: 50,
+    marginBottom: 50,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontFamily: 'Quicksand_700Bold',
+    color: '#FFF',
+    fontSize: 18,
 
-    },
-    containerForm:{
-        flex:1,
-        borderTopLeftRadius:25,
-        borderTopRightRadius: 25,
-        paddingStart: '5%',
-        padding: 16, // Em vez de 'paddingStart' e 'paddingEnd'
-    },
-    input:{
-        paddingLeft:10,
-        borderRadius:15,
-        backgroundColor:'#865FD6',
-        height:30,
-        marginBottom: 12,
-        fontSize:16, 
-        textAlign:"left"
-    },
-    inputDesc:{
-        backgroundColor:'#865FD6',
-        borderRadius:15,
-        height:100,
-        marginBottom: 12,
-        fontSize:16 
-    },
-    title:{
-        fontFamily:'Quicksand_400Regular',
-        fontSize: 20,
-        marginTop:28
-    },
-    containerButtons:{
-        flexDirection: 'row'
-    },
-    dateComponent:{
-        width:350,
-        color: '#865FD6',
-        backgroundColor: '#FFFFF'
-    },
-    textHeader: {
-        fontSize: 36,
-        marginVertical: 60,
-        color: "#111",
-      },
-      textSubHeader: {
-        fontSize: 25,
-        color: "#111",
-      },
-      inputBtn: {
-        borderWidth: 1,
-        borderRadius: 4,
-        borderColor: "#222",
-        height: 50,
-        paddingLeft: 8,
-        fontSize: 18,
-        justifyContent: "center",
-        marginTop: 14,
-      },
-      centeredView: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      modalView: {
-        margin: 20,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 20,
-        padding: 35,
-        width: "90%",
-      }
+
+  },
+  containerForm: {
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    padding: 16, // Em vez de 'paddingStart' e 'paddingEnd'
+  },
+  input: {
+    paddingLeft: 10,
+    borderRadius: 15,
+    backgroundColor: '#865FD6',
+    height: 30,
+    marginBottom: 12,
+    fontSize: 16,
+    textAlign: "left"
+  },
+  inputDesc: {
+    backgroundColor: '#865FD6',
+    borderRadius: 15,
+    height: 100,
+    marginBottom: 12,
+    fontSize: 16
+  },
+  title: {
+    fontFamily: 'Quicksand_400Regular',
+    fontSize: 20,
+    marginTop: 28
+  },
+  containerButtons: {
+    flexDirection: 'row'
+  },
+  dateComponent: {
+    width: 350,
+    color: '#865FD6',
+    backgroundColor: '#FFFFF'
+  },
+  textHeader: {
+    fontSize: 36,
+    marginVertical: 60,
+    color: "#111",
+  },
+  textSubHeader: {
+    fontSize: 25,
+    color: "#111",
+  },
+  inputBtn: {
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#222",
+    height: 50,
+    paddingLeft: 8,
+    fontSize: 18,
+    justifyContent: "center",
+    marginTop: 14,
+  },
+  centeredView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalView: {
+    margin: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    padding: 35,
+    width: "90%",
+  }
 });
 
 export default SendFotos;
